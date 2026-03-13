457882bcf2b6f37c1817e388f77f4e41cfea550a
Renan Câmara <renan.camara00@gmail.com>
2026-03-13 10:41:06 -0300

    Delete Exercicio4.js

--- Exercicio4.js
+++
@@ -1,7 +0,0 @@
-function DividendoDivisor(dv, ds) {
-    let resultado = dv / ds
-    let resto = dv % ds
-    return `Resultado dessa divisão: ${Math.floor(resultado)}, e o resto: ${resto}`
-}
-
-console.log(DividendoDivisor(11,4))
